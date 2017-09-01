4218947553bf5eede2e0c244c3a85e35b56aa58e
Justin Preece <wjpin84@gmail.com>
2017-09-01 14:41:06 +0000

    added motd and changed apache defautl for node attributes.

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,12 +4,19 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 # Basic apache installation and service starting.
+
+if node['platform_family'] == 'rhel'
+  apache_pkg = "httpd"
+elsif node['platform_family'] == 'debian'
+  apache_pkg = "apache2"
+end
+
 package 'apache2' do
-  package_name 'httpd'
+  package_name apache_pkg
   action :install
 end
 
 service 'apache2' do
-  service_name 'httpd'
+  service_name apache_pkg
   action [:enable, :start]
 end
